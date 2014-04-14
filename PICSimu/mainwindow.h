d18264ad783a31916c48f15ca1392dcff61b35ea
waxmar <mariowaxenegger@gmail.com>
2014-04-14 16:49:59 +0200

    header

--- PICSimu/mainwindow.h
+++ PICSimu/mainwindow.h
@@ -30,8 +30,8 @@ private:
     Ui::MainWindow *ui;
     void initializeSpeicherWidget(void);
     void initializeSFRWidget(void);
-    QString convertIntToBinString(int bank,int file);
-    QString convertIntToHexString(int bank,int file);
+    QString convertFileToBinString(int bank,int file);
+    QString convertFileToHexString(int bank,int file);
 private slots:
     void slotLoadLstFile(void);
     void slotLoad_FileDialog(void);
