419173c7caf5d519358bfa78494071933536ff1c
waxmar <mariowaxenegger@gmail.com>
2014-05-05 16:31:40 +0200

    RRF fixed

--- PICSimu/prozessor.cpp
+++ PICSimu/prozessor.cpp
@@ -402,14 +402,22 @@ void Prozessor::rrf(int command)
 
     // Register laden
     int currentValue = speicher.read(file);
+    int statusValue = speicher.read(0x03);  // Status-Register laden für Carry-Flag
     if(currentValue== 0x0100) //die Speicheradresse ist nicht belegt!!
     {
         cycles++;
         return;
     }
 
+    int carryFlag = 0;
+
+    if( CHECK_BIT(statusValue, 0) )
+        carryFlag = 0x80;               // carryFlag = 1000 0000
+
+
     // Operation
     int newValue = currentValue >>  1;
+    newValue |= carryFlag;
 
     // betroffene Flags prüfen und setzen/löschen
     checkCarryFlagRRF(currentValue);
