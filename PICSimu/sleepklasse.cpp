298ceeb98759e89721660c3e8e2311a194de7ec0
Benedikt1992 <slider25@web.de>
2014-05-10 19:54:44 +0200

    PAP sleep

--- PICSimu/sleepklasse.cpp
+++ PICSimu/sleepklasse.cpp
@@ -20,14 +20,14 @@ void SleepKlasse::run()
     int currentStatus=sW->alu->speicher.readOnBank(0,3); //Lese Status register
     currentStatus = (currentStatus | 0x0010) & 0xfff7;  //clear PD and set TO bit in Status register
     sW->alu->speicher.writeOnBank(0,3,currentStatus);
-cout << sW->wdt << endl;
+
     while(sW->isSleeping())
     {
         sW->alu->vorherigeCycles = sW->alu->cycles;
 
         sW->alu->nop();
         sW->countWDT();
-        cout << sW->wdt << endl;
+
         emit sW->refreshRuntime();
 
         if(sW->isWDTTimeOut())
